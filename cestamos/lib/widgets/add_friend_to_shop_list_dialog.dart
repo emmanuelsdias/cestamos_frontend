6cd1219481c1e479f001fbadf9b92b0ccf677957
GaGandour <gagandour@gmail.com>
2022-11-26 21:17:28 -0300

    list options

---
+++ cestamos/lib/widgets/add_friend_to_shop_list_dialog.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './form_buton.dart';
+import '../providers/friendships.dart';
+import '../models/friendship.dart';
+
+class AddFriendToShopListDialog extends StatefulWidget {
+  const AddFriendToShopListDialog({
+    required this.addFriend,
+    Key? key,
+  }) : super(key: key);
+
+  final Function addFriend;
+
+  @override
+  State<AddFriendToShopListDialog> createState() =>
+      _AddFriendToShopListDialogState();
+}
+
+class _AddFriendToShopListDialogState extends State<AddFriendToShopListDialog> {
+  @override
+  Widget build(BuildContext context) {
+    final friendshipsData = Provider.of<Friendships>(context);
+    final friendships = friendshipsData.friendships;
+    return Dialog(
+      child: Container(
+        color: Colors.white,
+        width: MediaQuery.of(context).size.width * 0.7,
+        height: MediaQuery.of(context).size.height * 0.7,
+        child: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Text(
+                "Amigos",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemBuilder: (_, index) {
+                  Friendship friendship = friendships[index];
+                  return GestureDetector(
+                    child: ListTile(
+                      title: Text(friendship.username),
+                    ),
+                    onTap: () {
+                      _showConfirmDialog(context, friendship);
+                    },
+                  );
+                },
+                itemCount: friendships.length,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _showConfirmDialog(BuildContext context, Friendship friendship) {
+    showDialog(
+      context: context,
+      builder: (_) {
+        return Dialog(
+          child: Container(
+            color: Colors.white,
+            width: MediaQuery.of(context).size.width * 0.6,
+            height: 220,
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  Text(
+                    "Quer adicionar ${friendship.username} à lista de compras?",
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(),
+                  ),
+                  FormButton(
+                      text: "Sim",
+                      icon: Icons.add,
+                      onPressed: () {
+                        widget.addFriend(friendship);
+                        Navigator.of(context).pop();
+                        Navigator.of(context).pop();
+                      }),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  FormButton(
+                    text: "Não",
+                    icon: Icons.add,
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    option: 2,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ cestamos/lib/widgets/confirm_quit_shop_list_dialog.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+import './form_buton.dart';
+
+class ConfirmQuitShopListDialog extends StatelessWidget {
+  const ConfirmQuitShopListDialog({
+    required this.quit,
+    Key? key,
+  }) : super(key: key);
+
+  final Function quit;
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width * 0.7,
+        height: 300,
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text(
+                "Tem certeza que quer sair da lista?",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              FormButton(
+                text: "Não",
+                icon: Icons.cancel,
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                option: 2,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              FormButton(
+                text: "Sim",
+                icon: Icons.exit_to_app_outlined,
+                onPressed: quit,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
