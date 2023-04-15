56165c5c9bcdec7851d42eb63f608c900de31ee8
asclaudino <arielsclaudino@gmail.com>
2023-04-15 16:21:44 -0300

    implemented models for recipes, started recipe feed

--- cestamos/lib/pages/recipes_page.dart
+++ cestamos/lib/pages/recipes_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../widgets/cestamos_bar.dart';
+import '../models/recipe.dart';
 // import '../helpers/http-requests/user.dart';
 // import '../models/user.dart';
 
@@ -12,6 +13,8 @@ class RecipesPage extends StatefulWidget {
 }
 
 class _RecipesPageState extends State<RecipesPage> {
+  List<RecipeSummary> _recipes = []; // TEM QUE CRIAR O MODEL DA RECEITA
+
   Future<bool> _refreshRecipes() async {
     // var response = ShopListHttpRequestHelper.getLists();
     // return response.then((value) {
@@ -38,16 +41,50 @@ class _RecipesPageState extends State<RecipesPage> {
         ],
       ),
       backgroundColor: Theme.of(context).colorScheme.background,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'Minhas receitas aqui ficarão aqui!',
-            ),
-          ],
-        ),
-      ),
+      // body: Center(
+      //   child: Column(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: const <Widget>[
+      //       Text(
+      //         'Minhas receitas aqui ficarão aqui!',
+      //       ),
+      //     ],
+      //   ),
+      // ),
+      body: FutureBuilder<bool>(
+          future: _refreshRecipes(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            var success = snapshot.data!;
+            if (!success) {
+              return const Center(
+                child: Text(
+                    "Algo de errado aconteceu nas suas receitas. Tente novamente mais tarde!"),
+              );
+            }
+            return _recipes.isEmpty
+                ? const Center(
+                    child: Text("Você ainda não tem receitas!"),
+                  )
+                : Column(
+                    children: const [
+                      Padding(
+                        padding: EdgeInsets.all(15),
+                        child: Text(
+                          "Minhas Receitas",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25,
+                          ),
+                        ),
+                      )
+                    ],
+                  );
+          }),
     );
   }
 }
